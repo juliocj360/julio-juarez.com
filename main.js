9463afe6e1cb65117dabde6570aa35fdb9cafefa
juliocj360 <juliocj360@gmail.com>
2017-07-11 21:51:10 -0700

    Current progress for button scroll. #28

--- main.js
+++ main.js
@@ -133,3 +133,31 @@ const formSubmit = () => {
     LPAWS.sendToTopic(params)
   }
 }
+
+const test = document.getElementById("test");
+
+test.onclick = function() {
+  TweenMax.to(window, 1, {scrollTo:{y:"#about-section", autoKill:false}, ease:Strong.easeOut})
+  const contButton = document.getElementById('fave-text')
+  console.log(isElementVisible(contButton))
+}
+
+function isElementVisible(el) {
+    var rect     = el.getBoundingClientRect(),
+        vWidth   = window.innerWidth || doc.documentElement.clientWidth,
+        vHeight  = window.innerHeight || doc.documentElement.clientHeight,
+        efp      = function (x, y) { return document.elementFromPoint(x, y) };
+
+    // Return false if it's not in the viewport
+    if (rect.right < 0 || rect.bottom < 0
+            || rect.left > vWidth || rect.top > vHeight)
+        return false;
+
+    // Return true if any of its four corners are visible
+    return (
+          el.contains(efp(rect.left,  rect.top))
+      ||  el.contains(efp(rect.right, rect.top))
+      ||  el.contains(efp(rect.right, rect.bottom))
+      ||  el.contains(efp(rect.left,  rect.bottom))
+    );
+}
